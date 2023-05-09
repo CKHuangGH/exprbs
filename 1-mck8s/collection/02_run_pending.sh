ac69ece487df179daaa2fe32d8f4c46734097693
CKHuangGH <chihkaihuang1@gmail.com>
2023-05-09 14:30:47 +0200

    up new

--- 1-mck8s/collection/02_run_pending.sh
+++ 1-mck8s/collection/02_run_pending.sh
@@ -7,13 +7,13 @@ break
 done < node_list_all
 
 #python3 ./script/scrapetime.py &
-#. ./script/toppodd.sh > /dev/null &
-. ./script/toppodkf.sh > /dev/null &
+. ./script/toppodd.sh > /dev/null &
+#. ./script/toppodkf.sh > /dev/null &
 #. ./script/toppodks.sh > /dev/null &
 . ./script/topnode.sh 0 > /dev/null &
 #. ./script/toppodm.sh 0 > /dev/null &
 
-ssh root@10.$ip1.$ip2.3 timeout 6000 tcpdump -i ens3 tcp and dst 10.158.0.3 and not src 10.158.0.2  -nn -q >> cross  &
+ssh root@10.$ip1.$ip2.3 timeout 6000 tcpdump -i ens3 tcp and dst 10.$ip1.$ip2.3 and not src 10.$ip1.$ip2.2  -nn -q >> cross  &
 
 j=1
 for i in $(cat node_list)
@@ -26,8 +26,8 @@ do
 	ssh root@$i . /root/exprbs/1-mck8s/collection/script/getpod.sh $j > /dev/null &
 	j=$((j+1))	
 done
-python3 /root/k8s_google_injection/run_deployments_jobs_kubefed.py &
-#python3 /root/k8s_google_injection/run_deployments_jobs_mck8s_worst_fit.py &
+#python3 /root/k8s_google_injection/run_deployments_jobs_kubefed.py &
+python3 /root/k8s_google_injection/run_deployments_jobs_mck8s_worst_fit.py &
 #python3 /root/k8s_google_injection/run_deployments_jobs_low.py &
 echo "wait for 7200 secs"
 sleep 7200
