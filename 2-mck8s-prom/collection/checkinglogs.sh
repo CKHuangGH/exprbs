567c207d9b5463fa8a31822b1ec410b072f2001a
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-05-12 22:45:42 +0200

    update

--- 2-mck8s-prom/collection/checkinglogs.sh
+++ 2-mck8s-prom/collection/checkinglogs.sh
@@ -8,7 +8,7 @@ do
     echo $logs
 
     if [ "$logs" = "$lastlogs" ]; then
-	    . /root/mck8s_vm/sasi/sasi/delmcs.sh $j
+	    . /root/exprbs/2-mck8s-prom/collection/delmcs.sh $j
         if [ $j -eq 3 ]; then
             break
         fi
