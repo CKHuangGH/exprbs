0c2e24bc390f3e4153a10cc0f610397d16998be4
CKHuangGH <chihkaihuang1@gmail.com>
2023-06-09 15:38:40 +0200

    Create 00_setup.sh

---
+++ single/00_setup.sh
@@ -0,0 +1,33 @@
+apt-get update
+sudo apt-get install vim -y
+sudo apt-get install net-tools -y
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+
+curl -s https://raw.githubusercontent.com/karmada-io/karmada/master/hack/install-cli.sh | sudo bash
+
+curl -s https://raw.githubusercontent.com/karmada-io/karmada/master/hack/install-cli.sh | sudo bash -s kubectl-karmada
+
+curl -L https://raw.githubusercontent.com/open-cluster-management-io/clusteradm/main/install.sh | bash
+
+# Install helm3
+echo "Helm3"
+wget -c https://get.helm.sh/helm-v3.8.2-linux-amd64.tar.gz
+tar xzvf helm-v3.8.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/
+helm repo add stable https://charts.helm.sh/stable
+helm repo add cilium https://helm.cilium.io/
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+curl --fail -LS "https://github.com/liqotech/liqo/releases/download/v0.8.3/liqoctl-linux-amd64.tar.gz" | tar -xz
+sudo install -o root -g root -m 0755 liqoctl /usr/local/bin/liqoctl
+
+
+wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+go install sigs.k8s.io/kind@v0.19.0
+
+cp /root/go/bin/kind /usr/local/bin/kind
