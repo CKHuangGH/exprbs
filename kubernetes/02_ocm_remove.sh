a70ee571c7d6cf17210078eb764494efb15047d3
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-07-02 23:31:27 +0200

    test

--- kubernetes/02_ocm_remove.sh
+++ kubernetes/02_ocm_remove.sh
@@ -3,3 +3,5 @@
 # Deploy Prometheus Federation on Cluster 0
 kubectl config use-context cluster0
 clusteradm clean --context cluster0
+
+kubectl delete ns cluster1
--- kubernetes/03_cicd_clusteradm.sh
+++ kubernetes/03_cicd_clusteradm.sh
@@ -2,4 +2,9 @@
 
 # Deploy Prometheus Federation on Cluster 0
 kubectl config use-context cluster0
-clusteradm clean --context cluster0
+for i in `seq 0 1`
+do
+    kubectl config use-context cluster$i
+	helm repo update
+	helm install cilium cilium/cilium --version 1.13.4 --wait --wait-for-jobs --namespace kube-system --set cluster.name=cluster$i --set cluster.id=$i
+done
