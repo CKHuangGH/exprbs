647b77c61e559395f086cf36f6be8f5e9ccc325a
CKHuangGH <chihkaihuang1@gmail.com>
2023-07-03 16:09:35 +0200

    Update combineAll.sh

--- kubernetes/combineAll.sh
+++ kubernetes/combineAll.sh
@@ -77,10 +77,8 @@ helm repo update
 
 echo "install go"
 wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
-sleep 3
+tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+cp /usr/local/go/bin/go /usr/local/bin
 
 for i in `seq 0 0`
 do
