744afb9409ef2c363f4d8ffdcbd88c66f140935e
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-02 17:56:17 +0200

    Update 02_ocm.sh

--- kubernetes/02_ocm.sh
+++ kubernetes/02_ocm.sh
@@ -3,6 +3,6 @@
 # Deploy Prometheus Federation on Cluster 0
 kubectl config use-context cluster0
 
-clusteradm init --wait --context cluster0
-clusteradm init --wait --context cluster1
-clusteradm init --wait --context cluster2
+clusteradm init --wait --context cluster0 &
+clusteradm init --wait --context cluster1 &
+clusteradm init --wait --context cluster2 &
