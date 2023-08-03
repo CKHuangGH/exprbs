999d2bda51689445d22d5d4f1f73d5d5b162e5fd
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-03 17:07:14 +0200

    test

--- kubernetes/03_google.sh
+++ kubernetes/03_google.sh
@@ -1,13 +1,14 @@
-clusteradm create work adservice -f google-demo/adservice.yaml --clusters cluster1
-clusteradm create work cartservice -f google-demo/cartservice.yaml --clusters cluster1
-clusteradm create work checkoutservice -f google-demo/checkoutservice.yaml --clusters cluster1
-clusteradm create work currencyservice -f google-demo/currencyservice.yaml --clusters cluster1
-clusteradm create work emailservice -f google-demo/emailservice.yaml --clusters cluster1
-clusteradm create work frontend -f google-demo/frontend.yaml --clusters cluster1
-clusteradm create work kustomization -f google-demo/kustomization.yaml --clusters cluster1
-clusteradm create work loadgenerator -f google-demo/loadgenerator.yaml --clusters cluster1
-clusteradm create work paymentservice -f google-demo/paymentservice.yaml --clusters cluster1
-clusteradm create work productcatalogservice -f google-demo/productcatalogservice.yaml --clusters cluster1
-clusteradm create work recommendationservice -f google-demo/recommendationservice.yaml --clusters cluster1
-clusteradm create work redis -f google-demo/redis.yaml --clusters cluster1
-clusteradm create work shippingservice -f google-demo/shippingservice.yaml --clusters cluster1
+# clusteradm create work adservice -f google-demo/adservice.yaml --clusters cluster1
+# clusteradm create work cartservice -f google-demo/cartservice.yaml --clusters cluster1
+# clusteradm create work checkoutservice -f google-demo/checkoutservice.yaml --clusters cluster1
+# clusteradm create work currencyservice -f google-demo/currencyservice.yaml --clusters cluster1
+# clusteradm create work emailservice -f google-demo/emailservice.yaml --clusters cluster1
+# clusteradm create work frontend -f google-demo/frontend.yaml --clusters cluster1
+# clusteradm create work kustomization -f google-demo/kustomization.yaml --clusters cluster1
+# clusteradm create work loadgenerator -f google-demo/loadgenerator.yaml --clusters cluster1
+# clusteradm create work paymentservice -f google-demo/paymentservice.yaml --clusters cluster1
+# clusteradm create work productcatalogservice -f google-demo/productcatalogservice.yaml --clusters cluster1
+# clusteradm create work recommendationservice -f google-demo/recommendationservice.yaml --clusters cluster1
+# clusteradm create work redis -f google-demo/redis.yaml --clusters cluster1
+# clusteradm create work shippingservice -f google-demo/shippingservice.yaml --clusters cluster1
+clusteradm create work google-demo -f google-demo/kubernetes-manifests.yaml --clusters cluster1
