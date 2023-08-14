0e00a2a27e3a5145a8c0baa6cc6692ed5e6e4d35
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-14 19:39:19 +0200

    Update 02_ocm.sh

--- kubernetes/02_ocm.sh
+++ kubernetes/02_ocm.sh
@@ -4,5 +4,5 @@
 kubectl config use-context cluster0
 
 clusteradm init --wait --context cluster0 &
-clusteradm init --wait --context cluster1 &
+#clusteradm init --wait --context cluster1 &
 # clusteradm init --wait --context cluster2 &
