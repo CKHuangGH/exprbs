e8bc59dc0b0dad321d9cf6c154fea0fe9aaa40e1
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-22 23:18:29 +0200

    test

--- kubernetes/02_ocm.sh
+++ kubernetes/02_ocm.sh
@@ -3,6 +3,8 @@
 # Deploy Prometheus Federation on Cluster 0
 kubectl config use-context cluster0
 
-clusteradm init --wait --context cluster0 &
+clusteradm init --wait --context cluster0 > temp.sh
+grep "clusteradm join" temp.sh > run.sh
+./auto.sh
 #clusteradm init --wait --context cluster1 &
 # clusteradm init --wait --context cluster2 &
