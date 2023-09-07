a5c6e4207dfdc2c7dedda1768a553f281963207f
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-09-07 22:59:36 +0200

    ee

--- kubernetes/solo/run.sh
+++ kubernetes/solo/run.sh
@@ -1,3 +1,3 @@
-kubectl apply -f vcluster.yaml
+vcluster create vcluster-0 --chart-version v0.15.5
 
 kubectl get secret --all-namespaces -o json | jq -r '.items[] | select(.metadata.name == "external-managed-kubeconfig") | .data.kubeconfig' | base64 --decode > kubeconfig.yaml
