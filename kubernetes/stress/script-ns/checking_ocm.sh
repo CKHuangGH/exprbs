6999ea3c5e23c949ac966d476e88871e63f60d0e
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-10-03 19:54:00 +0200

    test

--- kubernetes/stress/script-ns/checking_ocm.sh
+++ kubernetes/stress/script-ns/checking_ocm.sh
@@ -1,6 +1,6 @@
 numberofpod=$1
 LABEL_SELECTOR="vcluster.loft.sh/managed-by=vcluster"
-NUM_PODS=$numberofpod  # 修改為您的Pod數量
+NUM_PODS=$numberofpod+1  # 修改為您的Pod數量
 SLEEP_INTERVAL=1  # 檢查狀態的間隔時間（秒）
 
 # 等待所有Pod都變成運行狀態
