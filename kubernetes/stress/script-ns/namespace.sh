12a66413530b1a46841b286acfc1d2b74af3cd00
CKHuangGH <chihkaihuang1@gmail.com>
2023-10-03 15:49:47 +0200

    namespace

---
+++ kubernetes/stress/script-ns/namespace.sh
@@ -0,0 +1,15 @@
+
+# 迴圈 500 次
+for ((i=1; i<=1000; i++)); do
+    deployment_yaml=$(cat <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: namespace-$i
+EOF
+)
+    clusteradm create work namespace$i -f - <<< "$deployment_yaml" --cluster cluster1
+	
+done
+echo "All deployments created." >> number.txt
+echo $(date +'%s.%N') >> number.txt
