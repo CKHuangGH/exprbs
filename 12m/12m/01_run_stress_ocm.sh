d16d064d68701c782d64e637b08e29c1e7720fb8
CKHuangGH <chihkaihuang1@gmail.com>
2023-10-09 16:41:55 +0200

    ok test

---
+++ 12m/12m/01_run_stress_ocm.sh
@@ -0,0 +1,30 @@
+cp ../node_list node_list
+cp ../node_list_all node_list_all
+mkdir results
+
+while read -r ip; do
+    # 忽略空行和注释行
+    if [[ "$ip" =~ ^[[:space:]]*$ || "$ip" =~ ^\s*# ]]; then
+        continue
+    fi
+
+    # 执行ping命令
+    ping -c 4 "$ip" > number.txt  # 这里的-c 4表示ping 4次，您可以根据需要更改
+done < "node_list"
+
+. ./script/topapi.sh > /dev/null &
+
+tcpdump -i ens3 port 6443 -nn -q >> cross &
+
+echo "waiting 180 secs......."
+sleep 180
+echo "start collect" >> number.txt
+echo $(date +'%s.%N') >> number.txt
+
+echo "wait for 9000 secs"
+for (( i=9000; i>0; i-- )); do
+    echo "$i secs..."
+    sleep 1
+done
+
+. 02.getdocker.sh $number
