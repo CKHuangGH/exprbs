5ff64070787fda3fcb9ca791d65538631f486245
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-10-11 01:19:25 +0200

    dd

--- 12m/00_deploy.sh
+++ 12m/00_deploy.sh
@@ -1,6 +1,6 @@
 cp node_list node_list_all
 sed -i '1d' node_list
-mv node_list ./mcluster_example/node_list
+cp node_list ./mcluster_example/node_list
 ls /root/.kube/
 read -p "please enter the last cluster number in .kube: " number
 
