0881163844cc1438e41aebeb8631cf5c8326a533
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-10-18 19:20:03 +0200

    Update check.sh

--- 12m/check.sh
+++ 12m/check.sh
@@ -6,6 +6,7 @@ do
   if [ $? -ne 0 ]; then
     echo "缺少命名空间: $ns"
 	cluster=$(awk "NR==$i" node_list)
+  awk "NR==$i" node_list
 	ssh root@$cluster helm uninstall cilium -n kube-system
 	ssh root@$cluster helm install cilium cilium/cilium --version 1.13.4 --wait --wait-for-jobs --namespace kube-system --set operator.replicas=1
 	ssh root@$cluster 'bash -s' < "run$i.sh"
