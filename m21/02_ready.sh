6e6d181c439764bcceea70532aadc89109e0300a
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-01 20:08:19 +0100

    Update 02_ready.sh

--- m21/02_ready.sh
+++ m21/02_ready.sh
@@ -11,6 +11,10 @@ do
 
     mv "$temp_file" "$input_file"
 
-    bash < "$input_file"
+    bash < "$input_file" &
+    echo $cluster
+    if ((cluster % 50 == 0)); then
+        echo "sleep 180secs..."
+        sleep 180
     cluster=$((cluster+1))
 done
