cca571bd7b902118273c89f5a77683facc9344ff
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-06 22:16:52 +0100

    Update change.sh

--- m21/change.sh
+++ m21/change.sh
@@ -4,7 +4,7 @@
 etcd_deployment_yaml_file=/etc/kubernetes/manifests/etcd.yaml
 
 # Add the `--quota-backend-bytes=8589934592` flag to the `etcd` container
-sed -i '/etcd:/a\    - --quota-backend-bytes=8589934592' $etcd_deployment_yaml_file
+sed -i '/snapshot-count=10000:/a\    - --quota-backend-bytes=8589934592' $etcd_deployment_yaml_file
 
 # Print the updated etcd deployment YAML file
 cat $etcd_deployment_yaml_file
