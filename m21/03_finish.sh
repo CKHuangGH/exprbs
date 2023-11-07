315ce7abc232dbde335dd036f30a5e0b6631ea22
CKHuangGH <chihkaihuang1@gmail.com>
2023-11-07 16:43:46 +0100

    Update 03_finish.sh

--- m21/03_finish.sh
+++ m21/03_finish.sh
@@ -3,14 +3,26 @@ do
     ssh root@$i clusteradm accept --clusters "cluster1"
 done
 echo "wait 10 secs"
-sleep 10
+
+seconds=60
+
+# 開始倒數
+for i in $(seq $seconds); do
+  # 輸出倒數計數
+  echo "剩餘 $i 秒"
+  # 延遲 1 秒
+  sleep 1
+done
 
 for i in $(cat node_list)
 do
 	echo $i
-    ssh root@$i kubectl get pod -A | awk '$3 != "Running" {print $0}'
+    ssh root@$i kubectl get pods -A  --field-selector status.phase!=Running
 done
 
+echo "number of vcluster" 
 kubectl get pod -A |grep vcluster-0 |wc -l
+echo "number of work-agent" 
 kubectl get pod -A |grep work-agent |wc -l
-kubectl get pod -A | awk '$3 != "Running" {print $0}'
+
+kubectl get pods -A  --field-selector status.phase!=Running
