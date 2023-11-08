1ea00ea232d915b2f232181f1b411dc4b9315848
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-08 22:36:15 +0100

    Update change.sh

--- m21/change.sh
+++ m21/change.sh
@@ -8,8 +8,8 @@ sed -i '/snapshot-count=10000/a\    - --quota-backend-bytes=8589934592' $etcd_de
 sed -i '/snapshot-count=10000/a\    - --auto-compaction-mode=revision' $etcd_deployment_yaml_file
 sed -i '/snapshot-count=10000/a\    - --auto-compaction-retention=1000' $etcd_deployment_yaml_file
 
-sed -i '/secure-port=6443/a\    - --max-requests-inflight=2000' $api_deployment_yaml_file
-sed -i '/secure-port=6443/a\    - --max-mutating-requests-inflight=1000' $api_deployment_yaml_file
+sed -i '/secure-port=6443/a\    - --max-requests-inflight=6000' $api_deployment_yaml_file
+sed -i '/secure-port=6443/a\    - --max-mutating-requests-inflight=3000' $api_deployment_yaml_file
 
 # Print the updated etcd deployment YAML file
 cat $etcd_deployment_yaml_file
