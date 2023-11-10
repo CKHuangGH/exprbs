f311dde8aec25609eff2d2274125e21f3fe3ee30
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-10 18:58:25 +0100

    Update 02_ready.sh

--- m21/02_ready.sh
+++ m21/02_ready.sh
@@ -1,4 +1,5 @@
 cluster=1
+start=1
 target="<cluster_name>"
 replacement="cluster1"
 for i in $(cat node_list)
@@ -14,8 +15,14 @@ do
     
     echo $cluster
     if ((cluster % 50 == 0)); then
-        sleep 15
+        sleep 60
         bash < "$input_file"
+        sed -n "${start},${cluster}p" "$file_path" | while IFS= read -r line; do
+        # 在这里执行你想要的操作，例如输出每一行的内容
+            echo "Processing line: $line"
+            ssh root@$line clusteradm accept --clusters "cluster1"
+        done
+        start=$cluster
     else
         bash < "$input_file" &
     fi
