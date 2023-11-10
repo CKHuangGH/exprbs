8279b19c806424c3f382926eb8788f6294ac77c7
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-10 23:09:29 +0100

    Update 02_ready.sh

--- m21/02_ready.sh
+++ m21/02_ready.sh
@@ -18,13 +18,15 @@ do
         sleep 60
         bash < "$input_file"
         cluster2=$((cluster+1))
-        sed -n "${start},${cluster2}p" "node_list" | while IFS= read -r line; do
-            echo "Processing line: $line"
-            ssh root@$line clusteradm accept --clusters "cluster1"
+        for ((i=$start; i<=$cluster; i++)); do
+            current_line=$(sed -n "${i}p" "node_list")
+            echo "Processing line: $current_line"
+            ssh root@$current_line clusteradm accept --clusters "cluster1"
         done
         start=$cluster
     else
         bash < "$input_file" &
     fi
     cluster=$((cluster+1))
+    sleep 120
 done
