1a17a8ee0d681b64218e87c922080407bdc50bcd
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-11-13 22:02:13 +0100

    Update 02_ready.sh

--- m21/02_ready.sh
+++ m21/02_ready.sh
@@ -31,3 +31,16 @@ do
     cluster=$((cluster+1))
     
 done
+
+for i in $(cat node_list)
+do
+	echo $i
+    ssh root@$i kubectl get pods -A  --field-selector status.phase!=Running
+done
+
+echo "number of vcluster" 
+kubectl get pod -A |grep vcluster-0 |wc -l
+echo "number of work-agent" 
+kubectl get pod -A |grep work-agent |wc -l
+
+kubectl get pods -A  --field-selector status.phase!=Running
