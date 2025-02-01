2648f3f39cb215b569c818e37a0a762b978f1a70
CKHuang-public <95505429+CKHuangGH@users.noreply.github.com>
2025-02-01 01:52:00 +0100

    ww

---
+++ edgesys/121/auto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+target="<cluster_name>"
+replacement="cluster1"
+
+input_file="run.sh"
+
+temp_file="temp_file.sh"
+
+sed "s/$target/$replacement/g" "$input_file" > "$temp_file"
+
+mv "$temp_file" "$input_file"
+
+for i in $(cat node_list)
+do
+	ssh root@$i 'bash -s' < run.sh
+done
+
+sleep 10
+
+clusteradm accept --clusters cluster1
---
+++ edgesys/121/stress/script-ns/10000.sh
@@ -0,0 +1,32 @@
+
+# 迴圈 1000 次
+for ((i=1; i<=1000; i++)); do
+    deployment_yaml=$(cat <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-$i
+  namespace: namespace-$i
+  labels:
+    app: nginx
+spec:
+  replicas: 10
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+        - name: nginx
+          image: docker.io/library/nginx:latest
+          imagePullPolicy: Never
+EOF
+)
+    clusteradm create work test$i -f - <<< "$deployment_yaml" --cluster cluster1
+	
+done
+echo "All deployments created." >> number.txt
+echo $(date +'%s.%N') >> number.txt
