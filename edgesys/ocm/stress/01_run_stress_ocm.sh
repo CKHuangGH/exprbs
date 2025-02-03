1924e4e414c415758a4d7097fb98d55de65c0713
CKHuang-public <95505429+CKHuangGH@users.noreply.github.com>
2025-02-03 10:37:12 +0100

    ee

--- edgesys/ocm/stress/01_run_stress_ocm.sh
+++ edgesys/ocm/stress/01_run_stress_ocm.sh
@@ -56,7 +56,7 @@ do
 	j=$((j+1))
 done
 
-sudo tcpdump -i ens3 -nn -q '(src net 10.176.0.0/16 and dst net 10.176.0.0/16) and not arp' >> cross &
+sudo tcpdump -i ens3 -nn -q '(src net 10.144.0.0/16 and dst net 10.144.0.0/16) and not arp' >> cross &
 
 
 echo "wait for 18900 secs"
